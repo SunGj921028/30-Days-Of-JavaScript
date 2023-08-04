55d6cb02ff93cbb0bab1860150d43ba618918645
SunGj921028 <sungj921028@gmail.com>
2023-08-04 19:10:53 +0800

    Day 2
    practice

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -1,1 +1,31 @@
 // this is your main.js script
+
+let challenge = '30 Days Of JavaScript';
+console.log(challenge);
+console.log(challenge.length);
+
+console.log(challenge.toUpperCase());
+console.log(challenge.toLowerCase());
+
+console.log(challenge.substr(0,2));
+console.log(challenge.substring(3));
+
+console.log(challenge.includes('Script'));
+console.log(challenge.split());
+console.log(challenge.split(' '));
+
+let str = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+console.log(str.split(','));
+
+console.log(challenge.replace('JavaScript','Python'));
+
+console.log(challenge.charAt(15));
+
+console.log(challenge.charCodeAt(challenge.indexOf('J')));
+
+console.log(challenge.indexOf('a'));
+
+console.log('The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.');
+
+console.log(parseFloat('9.8'));
+console.log(Math.round(parseFloat('9.8')));
