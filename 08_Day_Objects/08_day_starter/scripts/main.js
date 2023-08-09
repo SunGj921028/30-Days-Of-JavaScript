78d53fcc32e86e1492e7834ee963175360dd1f11
SunGj921028 <sungj921028@gmail.com>
2023-08-10 00:34:01 +0800

    Day 8 Object
    8/10

--- 08_Day_Objects/08_day_starter/scripts/main.js
+++ 08_Day_Objects/08_day_starter/scripts/main.js
@@ -1,2 +1,40 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// console.log(countries)
+// alert('Open the console and check if the countries has been loaded')
+
+// //scope.js
+// let a = 'JavaScript' // is a global scope it will be found anywhere in this file
+// let b = 10 // is a global scope it will be found anywhere in this file
+// function letsLearnScope() {
+//   console.log(a, b) // JavaScript 10, accessible
+//   if (true) {
+//     let a = 'Python'
+//     let b = 100
+//     console.log(a, b) // Python 100
+//   }
+//   console.log(a, b) // JavaScript 10
+// }
+// letsLearnScope()
+// console.log(a, b) // JavaScript 10, accessible
+
+const person = {
+    firstName: 'Asabeneh',
+    age: 250,
+    country: 'Finland',
+    city:'Helsinki',
+    skills: ['HTML', 'CSS', 'JS'],
+    title: 'teacher',
+    address: {
+      street: 'Heitamienkatu 16',
+      pobox: 2002,
+      city: 'Helsinki'
+    },
+    getPersonInfo: function() {
+      return `I am ${this.firstName} and I live in ${this.city}, ${this.country}. I am ${this.age}.`
+    }
+  }
+  
+  //Object methods: Object.assign, Object.keys, Object.values, Object.entries
+  //hasOwnProperty
+  
+  const copyPerson = Object.assign({}, person)
+  console.log(copyPerson)
